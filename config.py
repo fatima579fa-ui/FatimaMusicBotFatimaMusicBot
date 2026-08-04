cb09b3b31b43d400ceef33fa375eae71caf06d40
fatima579fa-ui <fatima579fa@gmail.com>
2026-08-05 00:51:14 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+API_ID = int(os.environ.get("API_ID", "123456"))
+API_HASH = os.environ.get("API_HASH", "your_api_hash_here")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token_here")
+OWNER_NAME = os.environ.get("OWNER_NAME", "your_username")
